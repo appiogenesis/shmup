d35bcb8973811075349fb291865574969334f592
Salzimus <salzimus@gmail.com>
2017-11-05 00:57:23 +0100

    Set up initial files

---
+++ sketch.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const CANVAS_WIDTH = 480,
+      CANVAS_HEIGHT = 800;
+
+function setup()
+{
+	createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+}
+
+function draw()
+{
+	background(0);
+}
